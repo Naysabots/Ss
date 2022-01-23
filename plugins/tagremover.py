6757d1551e670ac4edf8f5efa3a6f63a85f3171b
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-23 15:00:13 +0530

    Update tagremover.py

--- plugins/tagremover.py
+++ plugins/tagremover.py
@@ -11,11 +11,5 @@ from pyrogram import Client as NaysaBots
    
   
 @NaysaBots.on_message(filters.forwarded & filters.channel)
-async def nstag(bot, message):
-   try:
-       chat_id = message.chat.id
-       forward_msg = await message.copy(chat_id)
-       await message.delete()
-   except:
-       await message.reply_text("Check My Admin Permissions and Try again")
+async def restricted(bot, message):
     
